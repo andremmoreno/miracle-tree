b7262ed6c80b8d5ee7c2ca4ee7616778bbd4f339
Andre Moreno <demoreno2@hotmail.com>
2025-01-16 21:47:22 +0100

    feat: add navbar and pages

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+
+  const path = usePathname()
+
+  return (
+    <header className="px-5 py-3 bg-neutral-800">
+      <nav className="flex justify-center items-center gap-4">
+        <Link href={"/"} className={path === "/" ? "" : "active"}>
+          Home
+        </Link>
+        <Link href={"/about"} className={path === "/about" ? "" : "active"}>
+          About Us
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
